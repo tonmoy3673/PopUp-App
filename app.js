8515d830140158a1416fe884c7acd1eb548b06c7
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-03-13 15:31:02 +0600

    :tada: added animation

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('connected');
+let popUp= document.getElementById('pop-up');
+
+// ========= display popUp =======//
+const openPopUp=()=>{
+   popUp.style.display='block';
+}
+
+// ============ Hide popUp ===========//
+const closePopUp=()=>{
+    popUp.style.display='none'
+}
